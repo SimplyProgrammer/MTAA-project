e2d42f0bfe5197395c32750b3242e3298267afe7
Programmer001 <petko.hupka@gmail.com>
2025-04-08 17:40:57 +0200

    swagger to json

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,6 +37,10 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+app.get("/api-docs.json", (req, res) => {	
+	res.json(swaggerSpec);
+});
+
 const PORT = process.env.PORT | 5000
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
