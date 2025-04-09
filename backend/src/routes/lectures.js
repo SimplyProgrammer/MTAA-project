bb85fd678b4ec1bafbefd79ae504f21f2a363aa5
simonkubis <simon.kubis20@gmail.com>
2025-04-09 23:15:51 +0200

    nove endpointy

---
+++ backend/src/routes/lectures.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const db = require("../config/db"); // Use the same pool
+
+
+module.exports = router;
---
+++ backend/src/routes/subjects.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const db = require("../config/db"); // Use the same pool
+
+
+
+module.exports = router;
