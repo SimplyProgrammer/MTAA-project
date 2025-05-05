d3d2065de2c8852502ab6457bc58883d508700ff
Programmer001 <petko.hupka@gmail.com>
2025-05-05 11:19:32 +0200

    posts mock

--- backend/sqls/mock.sql
+++ backend/sqls/mock.sql
@@ -5,10 +5,10 @@ SELECT * FROM useraccounts
 INSERT INTO Posts (user_id, title, text, image)
 SELECT
 	(random() * 2 + 1)::int,
-	'Post Title #' || gs, 
-	'This is the body of post #' || gs,
+	'The Post Title #' || gs, 
+	'This is the body of post # Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' || gs,
 	CASE WHEN random() < 0.5
-		THEN '/files/test.PNG'
+		THEN 'test.PNG'
 		ELSE NULL
 	END
 FROM generate_series(1, 50) AS gs;
